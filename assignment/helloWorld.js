a64dc4632ed63fea57793292d26ae82f48f64d50
John Hinderer <johnhinderer@Johns-MacBook-Air.local>
2020-07-15 13:45:37 -0500

    edited assignment 1

--- assignment/helloWorld.js
+++ assignment/helloWorld.js
@@ -1,13 +1,2 @@
- <!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="uft-8">
-    <title></title>
-  </head>
 
-  <body>
-    <script>
     console.log("Hello World");
-    </script>
-  </body>
-</html>
